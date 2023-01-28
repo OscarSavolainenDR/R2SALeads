0e1b9b9c15a80d0042cb766032bb274ce6354a61
Oscar <oscar.savolainen@hotmail.com>
2023-01-28 16:25:44 +0000

    Fixed requirements.txt

---
+++ delete.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Number of times to run the command
+num_iterations=1
+
+for ((i = 1; i <= num_iterations; i++)); do
+    # Run the command
+    rm backend_v3/.env
+    git add .
+    git commit --amend --no-edit
+    git rebase --continue
+
+    # Wait for some time (you can adjust the sleep duration as needed)
+    sleep 2
+done
