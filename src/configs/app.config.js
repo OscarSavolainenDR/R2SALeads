b4f0c1000d2b0df2d306bdbfdfb0ba18500f5f25
Oscar <oscar.savolainen@hotmail.com>
2023-03-14 21:18:50 +0000

    Why

--- src/configs/app.config.js
+++ src/configs/app.config.js
@@ -1,6 +1,6 @@
 const appConfig = {
     // apiPrefix: '/api',
-    apiPrefix: 'https://r2sa-v1.herokuapp.com/api/',
+    apiPrefix: 'https://r2sa-v1.herokuapp.com/api/', 
     // apiPrefix: 'http://localhost:8010/proxy/api',
     authenticatedEntryPath: '/home',
     unAuthenticatedEntryPath: '/sign-in',
