4655d7f12344fe09102e35120d66956abafd74a1
Oscar Savolainen <oscar.savolainen@deeprender.ai>
2024-02-03 16:22:32 +0000

    Temp

--- del.sh
+++ del.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 # Number of times to run the command
-<<<<<<< HEAD
-<<<<<<< HEAD
 num_iterations=100
-=======
-num_iterations=1
->>>>>>> 58e772b (first backend commit)
-=======
-num_iterations=100
->>>>>>> d60f568 (remove corsheaders for now)
 
 for ((i = 1; i <= num_iterations; i++)); do
     # Check if the file exists before removing it
     if [ -e backend_v3/.env ]; then
         rm backend_v3/.env
+    fi
+
+    if [ -e del.sh]; then
+        rm del.sh
     fi
 
     git add .
